4889cc437455d1da2cda20a457bb4830c5282e53
Oleg Khomyakov <homyakov46rus@yandex.ru>
2018-07-03 00:39:43 +0300

    first structure

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 // var middleware = require('./middleware')(app, express);
 
-app.listen(8080, function(){
+app.listen(process.env.PORT, function(){
     console.log('Express server listening');
 });
