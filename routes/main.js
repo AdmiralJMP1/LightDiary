0c37d731bb05758b3387b7e3b08f6d163c25f7e2
Oleg Khomyakov <homyakov46rus@yandex.ru>
2018-07-03 02:24:17 +0300

    auth test fix

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 module.exports = function(req, res) {
-   if(req.user)
-    res.send(req.user.username + ' from main');
-  else
-    res.send('unlogined from main');
+  //  if(req.user)
+  //   res.send(req.user.username + ' from main');
+  // else
+  //   res.send('unlogined from main');
+  res.send(process.env.GITHUB_CLIENT_ID)
 };
