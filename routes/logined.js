d14d343d1e6fb53267b3f929db3e298a804a7d6f
Oleg Khomyakov <homyakov46rus@yandex.ru>
2018-07-03 02:14:04 +0300

    auth test fix

--- routes/logined.js
+++ routes/logined.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res) {
   // console.log(req.user.username);
    if(req.user)
-    res.send(username: req.user.username + ' from log');
+    res.send(req.user.username + ' from log');
   else
     res.send('unlogined from log');
 };
--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res) {
    if(req.user)
-    res.send(username: req.user.username + ' from main');
+    res.send(req.user.username + ' from main');
   else
     res.send('unlogined from main');
 };
