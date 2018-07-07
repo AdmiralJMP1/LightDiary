edd43a2ef993e665b1822115158f8e1bb8c80faa
Oleg Khomyakov <homyakov46rus@yandex.ru>
2018-07-07 17:04:27 +0300

    es6 serever test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require('express');
+// var express = require('express');
+import express from 'express';
 var app = express();
 var middleware = require('./middleware')(app, express);
 var config = require("./config")();
